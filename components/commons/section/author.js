4762153105076eae8c67b03ea43a99a2c12837b1
Jevan Tang <22316055+jevantang@users.noreply.github.com>
2023-09-02 14:48:54 +0800

    chore: ip location

--- components/commons/section/author.js
+++ components/commons/section/author.js
@@ -3,7 +3,7 @@
  * Copyright 2021-Present 唐杰
  * Licensed under the Apache-2.0 license
  */
-import { fresnsLang } from '../../../api/tool/function';
+import { fresnsConfig, fresnsLang } from '../../../api/tool/function';
 import { globalInfo } from '../../../utils/fresnsGlobalInfo';
 
 Component({
@@ -13,6 +13,11 @@ Component({
     isAnonymous: Boolean,
     createdTimeAgo: String,
     location: Object,
+    ipLocation: String,
+    ipLocationShow: {
+      type: Boolean,
+      value: true,
+    },
   },
 
   /** 组件的初始数据 **/
@@ -20,6 +25,7 @@ Component({
     userHomePath: '',
     userDeactivate: null,
     authorAnonymous: null,
+    ipLocationStatus: false,
   },
 
   /** 组件生命周期声明对象 **/
@@ -29,6 +35,9 @@ Component({
         userHomePath: await globalInfo.userHomePath(),
         userDeactivate: await fresnsLang('userDeactivate'),
         authorAnonymous: await fresnsLang('contentAuthorAnonymous'),
+        ipLocationStatus: await fresnsConfig('account_ip_location_status'),
+        ipLocationDesc: await fresnsLang('ipLocation'),
+        errorIp: await fresnsLang('errorIp'),
       });
     },
   },
