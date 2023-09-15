56324636e5d4459e35010f967ef8cdaf0768f3b9
Jevan Tang <22316055+jevantang@users.noreply.github.com>
2023-09-15 11:55:22 +0800

    fix: cache

--- pages/account/index.js
+++ pages/account/index.js
@@ -101,7 +101,7 @@ Page({
 
   /** 监听页面渲染完成 **/
   onReady: async function () {
-    const appInfo = this.data.appInfo;
+    const appInfo = wx.getStorageSync('appInfo');
 
     if (appInfo.isWechat) {
       return;
