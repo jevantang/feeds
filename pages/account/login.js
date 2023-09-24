322c4208718563b2a2cfb0400cbd3250f4af59f3
Jevan Tang <22316055+jevantang@users.noreply.github.com>
2023-09-24 13:59:34 +0800

    style: code style

--- pages/account/login.js
+++ pages/account/login.js
@@ -133,8 +133,8 @@ Page({
           this.setData({
             hasWechatInstall: res.hasWechatInstall,
           });
-        }
-      })
+        },
+      });
     }
 
     if (options.showToast === 'true') {
